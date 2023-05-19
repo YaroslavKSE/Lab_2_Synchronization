d12f5ad8d83dab4288cf1bc292e5812be36fa37b
YaroslavKSE <ikhomych@kse.org.ua>
2023-05-19 10:17:02 +0300

    Fixed bugs in ThreadPool

--- Lab_2_Synchronization.cpp
+++ Lab_2_Synchronization.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include "TaskQueue.h"
 #include "ThreadPool.h"
+#include "TaskQueue.h"
 #include "Task.h"
 using namespace std;
 
@@ -8,19 +8,25 @@ using namespace std;
 
 
 int main() {
+	
 	TaskQueue firstQueue;
 	TaskQueue secondQueue;
 
 	Task t1;
 	Task t2;
 	Task t3;
-	firstQueue.emplace(t1);
-	firstQueue.emplace(t2);
-	secondQueue.emplace(t3);
+	//firstQueue.emplace(t1);
+	//firstQueue.emplace(t2);
+	//secondQueue.emplace(t3);
 
-	firstQueue.clear();
-	secondQueue.size();
+	//firstQueue.clear();
+	//secondQueue.size();
 
+	ThreadPool pool;
+	pool.initialize(5);
+	pool.add_task(t1);
+	pool.add_task(t2);
+	pool.add_task(t3);
 	return 0;
 
 }
